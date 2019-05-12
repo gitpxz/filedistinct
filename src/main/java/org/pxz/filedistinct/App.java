11f6cd6d53ed2d6a28be8e1a0256d3b49e6c6bb1
gitpxz <sccdpxzhi@qq.com>
2019-05-12 22:25:04 +0800

    提交备注201905122224

---
+++ src/main/java/org/pxz/filedistinct/App.java
@@ -0,0 +1,13 @@
+package org.pxz.filedistinct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
